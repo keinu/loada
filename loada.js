4e047cc6890d2c0276d0fc305f3ef08b98b0ac37
Xabi Errotabehere <github@keinu.net>
2015-04-26 15:12:25 +1000

    Linear progress added + animation improvement

--- loada.js
+++ loada.js
@@ -1,5 +1,7 @@
 var loada = (function() {
 
+	var loada;
+
 	var show = function() {
 		if (!loada.classList.contains("show")) {
 			loada.classList.add("show");
@@ -15,25 +17,40 @@ var loada = (function() {
 		if (percent > 100) {
 			percent = 100;
 		}
-
-		loada.setAttribute("style", "width: " + percent + "%");
+		loada.style.transitionDuration = "1s, 1s, 1s";
+		loada.style.width = percent + "%";
 		show();
 
 	};
 
-	var loada = document.createElement("div");
-		loada.setAttribute("id", "loada");
+	var linearProgress = function(totalDuration, remainingDuration) {
+
+		var initialProgress = 100 - (remainingDuration / totalDuration * 100);
+			initialProgress = initialProgress.toFixed(4);
+
+		go(initialProgress);
+		loada.style.transitionDuration = "1s, 1s, " + (remainingDuration / 1000) + "s";
+		setTimeout(function() {
+			loada.style.width = "100%";
+		}, 10);
+
+	};
+
+	loada = document.createElement("div");
+	loada.setAttribute("id", "loada");
 	document.body.insertBefore(loada, document.body.firstChild);
 
 	var styles = document.createElement("link");
 		styles.setAttribute("rel", "stylesheet");
-		styles.setAttribute("href", "https://cdn.rawgit.com/keinu/loada/master/css.css");
+		//styles.setAttribute("href", "https://cdn.rawgit.com/keinu/loada/master/css.css");
+		styles.setAttribute("href", "../css.css");
 	document.head.appendChild(styles);
 
 	return {
 		show: show,
 		hide: hide,
-		go: go
+		go: go,
+		linearProgress: linearProgress
 	};
 
 })();
